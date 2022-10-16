7c3638e52e8cce9a8c76c19b29f56436107a8d48
Jh0mpis <jhonsmorenot@gmail.com>
2022-10-16 00:51:03 -0500

    Cambios en Util y en Cripto Clásica

--- util.py
+++ util.py
@@ -1,17 +1,21 @@
 import random as rand
-from dataStructures import BinaryTree
+from dataStructures import BinaryTree, Queue
+import matplotlib.pyplot as mp
 
 def ROT(n,word):
     newWord=""
     lowerAbecedary = "abcdefghijklmnopqrstuvwxyz"
     upperAbecedary = lowerAbecedary.upper()
     for i in word:
-        if( i==i.upper()):
-            index = upperAbecedary.index(i)
-            newWord += upperAbecedary[(index+n)%len(upperAbecedary)]
+        if i == ' ':
+            newWord += i
         else:
-            index = lowerAbecedary.index(i)
-            newWord += lowerAbecedary[(index+n)%len(upperAbecedary)]
+            if(i==i.upper()):
+                index = upperAbecedary.index(i)
+                newWord += upperAbecedary[(index+n)%len(upperAbecedary)]
+            else:
+                index = lowerAbecedary.index(i)
+                newWord += lowerAbecedary[(index+n)%len(upperAbecedary)]
     return newWord
 
 def getRandomClasicBinaryTree(height,intMin=1,intMax=10):
@@ -29,3 +33,13 @@ def getNotRandomClasicBinaryTree(height,intLeft,intRight):
         tree.addKey(intLeft)
         tree.addKey(intRight)
     return tree
+
+def ROTTree(word,tree):
+    newWord = ""
+    path = ""
+    current = tree.root
+    for i in word:
+        current = rand.choice([current.left,current.right])
+        path += str(current.data)
+        newWord += ROT(current.data,i)
+    return newWord,path
